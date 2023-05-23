049afe7a5f19ecc4844571a4e3d8cefc955fdd28
Timothy Engkar <56295945+AsuraEyes@users.noreply.github.com>
2023-05-23 18:22:23 +0200

    changed uri

--- PresentationTier/Data/Media/MovieService.cs
+++ PresentationTier/Data/Media/MovieService.cs
@@ -6,7 +6,7 @@ namespace PresentationTier.Data.Media;
 public class MovieService : IMovieService
 {
     private readonly HttpClient client;
-    private const string uri = "https://localhost:7254/Movie";
+    private const string uri = "https://bestmoviesapi.azurewebsites.net/Movie";
 
     public MovieService()
     {
